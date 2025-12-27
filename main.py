c02f2f87773dbc3d5efc6f4a67c69ffbbcd33e0d
ZainfekowanyTypo <zainfekowany.jakub1@gmail.com>
2025-12-27 19:27:42 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,17 +2,14 @@ from modules.ui.main_window import MainWindow
 from modules.core import bindings as b
 from modules.core import settings as s
 
-
-# ------------------ Load Initial Data ------------------
-b.load_bindings()   # Load macro bindings & scram key
-s.load_settings()   # Load theme, replay mode, count
-
-# ------------------ Apply Theme ------------------
 import customtkinter as ctk
+
+b.load_bindings() 
+s.load_settings()
+
 ctk.set_appearance_mode(s.theme)
 ctk.set_default_color_theme("blue")
 
-# ------------------ Start Main Window ------------------
 if __name__ == "__main__":
     # Instantiate and run the UI
     app = MainWindow()
